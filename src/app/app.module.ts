8bc061171af3b6f1d6afa34b3b6d918ce25fc529
Nicolas Miranda <nicxesh@gmail.com>
2021-09-22 05:24:24 -0300

    Added some responsive components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// Router Module
 import { AppRoutingModule } from './app-routing.module';
 
 // Components
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-//import { KeyboardComponent } from './components/keyboard/keyboard.component';
 import { LogoComponent } from './components/logo/logo.component';
 
 @NgModule({
