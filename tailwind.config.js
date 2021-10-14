884853affad39e08598ab0db53ffecec61f2e266
Nicolás Miranda <nicxesh@gmail.com>
2021-10-14 12:49:01 -0300

    Added new typography family font

--- tailwind.config.js
+++ tailwind.config.js
@@ -24,6 +24,7 @@ module.exports = {
       fontFamily: {
         sans: ['Josefin Sans', 'sans-serif'],
         serif: ['Josefin Sans', 'serif'],
+        base: ['Poppins', 'sans-serif'],
       },
     },
     variants: {
